8bfda7bdc8e85f7488d2be0e4afe030347a2d892
alfarabi <alfarabidwik@gmail.com>
2019-09-26 15:41:03 +0700

    -

--- app/src/main/java/com/alfarabi/alfalibsapps/MainActivity.java
+++ app/src/main/java/com/alfarabi/alfalibsapps/MainActivity.java
@@ -1,49 +1,31 @@
 package com.alfarabi.alfalibsapps;
 
-import android.app.Activity;
 import android.os.Bundle;
-
-import com.alfarabi.alfalibs.helper.AlfaPagination;
-import com.alfarabi.alfalibs.http.HttpInstance;
-import com.alfarabi.alfalibs.http.mock.ExampleService;
-import com.alfarabi.alfalibs.http.mock.MockService;
-import com.alfarabi.alfalibs.model.UserModel;
-import com.alfarabi.alfalibs.tools.WLog;
-import com.alfarabi.alfalibs.views.AlfaRecyclerView;
-import com.alfarabi.alfalibs.views.AlfaSwipeRefreshLayout;
-import com.google.gson.Gson;
-
-import butterknife.BindView;
+import com.alfarabi.alfalibs.activity.APIBaseActivity;
 import butterknife.ButterKnife;
-import io.reactivex.Observable;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-public class MainActivity extends Activity {
+public class MainActivity extends APIBaseActivity {
     public static final String TAG = MainActivity.class.getName();
-
-//    @BindView(R.id.alfarefreshlayout) AlfaSwipeRefreshLayout alfarefreshlayout ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//        HttpInstance.sink(HttpInstance.fork(this, MockService.class).getUsers(1), userModels -> {
-//            WLog.i(TAG, new Gson().toJson(userModels));
-//        }, Throwable::printStackTrace);
-
-
-        HttpInstance.withProgress(this).observe(HttpInstance.create(MockService.class).getUser(1), userModel -> {
-
-        }, throwable -> {
-            throwable.printStackTrace();
-        });
     }
+
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public String getTAG() {
+        return TAG;
+    }
+
+    @Override
+    public int contentXmlLayout() {
+        return R.layout.activity_main;
     }
 }
